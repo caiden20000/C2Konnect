d0b9be453764aa3db1fd85b239723d813df99f7c
c w <caiden2000offline@gmail.com>
2024-11-05 07:09:26 +0000

    Basic express init

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,16 @@
-console.log("hello");
+import express from 'express';
+const app = express();
+import path from 'path';
+
+
+// Allow client to request CSS files on the ROOT HOST/
+app.use(express.static(path.join(__dirname, 'styles')));
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+    // res.redirect('/index');
+    // res.sendFile(__dirname + '/static/index.html');
+});
